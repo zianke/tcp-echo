5cf54f7cf62611643538278c62ad5a676104d7da
Zian Ke <zian.ke@zian.ke>
2020-06-19 16:01:50 -0400

    Log connection info

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 var (
@@ -15,6 +16,11 @@ var (
 	// stdoutW and stderrW are for overriding in test.
 	stdoutW = os.Stdout
 	stderrW = os.Stderr
+)
+
+const (
+	tcpLogDateFormat string = "2006/01/02 15:04:05"
+	tcpLogFormat     string = "%v [remote] %s %s [local] %s %s [duration] %v\n"
 )
 
 func main() {
@@ -32,11 +38,14 @@ func main() {
 		os.Exit(127)
 	}
 
+	// Create listener
 	ln, err := net.Listen("tcp", *listenFlag)
 	if err != nil {
 		log.Fatalf("[ERR] failed to create listener: %s", err)
 	}
 	log.Printf("[INFO] server is listening on %s\n", *listenFlag)
+
+	// Serve requests
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -47,6 +56,18 @@ func main() {
 }
 
 func handleConnection(conn net.Conn, v string) {
+	// Log connection info
+	defer func(start time.Time) {
+		end := time.Now()
+		dur := end.Sub(start)
+		fmt.Fprintf(stdoutW, tcpLogFormat,
+			end.Format(tcpLogDateFormat),
+			conn.RemoteAddr().String(), conn.RemoteAddr().Network(),
+			conn.LocalAddr().String(), conn.LocalAddr().Network(),
+			dur)
+	}(time.Now())
+
+	// Respond with text
 	fmt.Fprintln(conn, v)
 	conn.Close()
 }
